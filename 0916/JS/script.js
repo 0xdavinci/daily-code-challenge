7ddd034f2e6e5d40e96fdd722a93e71d4f17d776
0xdavinci <brianhowell117@gmail.com>
2025-09-19 12:51:03 -0400

    0916 Solved

---
+++ 0916/JS/script.js
@@ -0,0 +1,23 @@
+function capitalize(paragraph) {
+  let flag = true; // capitalize next letter?
+  const chars = paragraph.split(""); // strings are immutable in JS
+
+  for (let i = 0; i < chars.length; i++) {
+    const ch = chars[i];
+
+    // If we should capitalize and this is a lowercase ASCII letter, do: code-32
+    if (flag && ch >= "a" && ch <= "z") {
+      chars[i] = String.fromCharCode(ch.charCodeAt(0) - 32); // paragraph[i] = paragraph[i]-32
+      flag = false;
+    }
+
+    // After sentence-ending punctuation, capitalize next letter we see
+    if (ch === "." || ch === "?" || ch === "!") {
+      flag = true;
+    }
+  }
+
+  return chars.join("");
+}
+
+console.log(capitalize("this is a simple sentence."));
